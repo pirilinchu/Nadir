a38a342e242e982d7ad2fa3b6c7b442439ba90aa
Santiago Mendoza <santiagomendozaa6@gmail.com>
2020-10-27 11:01:50 -0400

    x

--- SkeletonExample/SkeletonExample/Pages/Page1.xaml.cs
+++ SkeletonExample/SkeletonExample/Pages/Page1.xaml.cs
@@ -15,10 +15,6 @@ namespace SkeletonExample.Pages
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            if (Device.RuntimePlatform.Equals(Device.iOS))
-                mainGrid.Margin = new Thickness(30, On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets().Top+30, 30, 30);
-            else
-                mainGrid.Margin = new Thickness(30, 50, 30, 30);
         }
 
 
